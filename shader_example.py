40af73c749f1c6b7d37b37be42f1dca5da74619e
Gabriel Brolo Tobar <gabrolo13@gmail.com>
2019-04-08 22:18:29 -0600

    Project start

--- shader_example.py
+++ shader_example.py
@@ -8,7 +8,7 @@ def init_renderer():
     GL.glInit()
     GL.glCreateWindow(1920, 1080)
     GL.glViewPort(0, 0, 1920, 1080)
-    GL.glClear(0, 0, 0)
+    GL.glClear(1, 1, 1)
     GL.glColor(1, 1, 1)
 
 ############################################### DRAWING FUNCTIONS ############################################
@@ -47,5 +47,76 @@ def high_shot():
 
     print('Output rendered to:  \'render.bmp\'')
 
+def forest():
+    # first obj
+    obj = 'forest/base.obj'
+    translate = (0, -0.8, -0.8)
+    scale = (0.1, 0.15, 0.1)
+    rotate = (0, 0, 0)
+    intensity = 1    
+    texture = texture_loader('forest/base.bmp')
+    print('Rendering:   ' + obj + '\ntranslate:   ' + str(translate) + '\nscale:   ' + str(scale))
+    print('Please wait...')
+    
+    init_renderer()
+    GL.glLookAt(
+        VERTEX_3(10, 25, 28), 
+        VERTEX_3(0, -0.2, 0), 
+        VERTEX_3(0, 1, 0)
+    )
+    GL.glLoadObj(obj, translate, scale, rotate, intensity, texture)
+
+    # second obj
+    obj = 'deer/deer_normals.obj'
+    translate = (-0.5, 0, 0)
+    scale = (0.08, 0.15, 0.08)
+    rotate = (0, 0, 0)
+    intensity = 1    
+    texture = texture_loader('deer/deer.bmp')
+    print('Rendering:   ' + obj + '\ntranslate:   ' + str(translate) + '\nscale:   ' + str(scale))
+    print('Please wait...')
+    
+    GL.glLoadObj(obj, translate, scale, rotate, intensity, texture)
+
+    # third obj
+    obj = 'forest/monolith.obj'
+    translate = (0.75, 0, 0)
+    scale = (0.1, 0.15, 0.1)
+    rotate = (0, 0, 0)
+    intensity = 1    
+    texture = texture_loader('forest/monolith.bmp')
+    print('Rendering:   ' + obj + '\ntranslate:   ' + str(translate) + '\nscale:   ' + str(scale))
+    print('Please wait...')
+    
+    GL.glLoadObj(obj, translate, scale, rotate, intensity, texture)
+
+    # fourth obj
+    obj = 'forest/rock.obj'
+    translate = (-0.3, 0.1, -0.45)
+    scale = (0.1, 0.15, 0.1)
+    rotate = (0, 0, 0)
+    intensity = 1    
+    texture = texture_loader('forest/rock.bmp')
+    print('Rendering:   ' + obj + '\ntranslate:   ' + str(translate) + '\nscale:   ' + str(scale))
+    print('Please wait...')
+    
+    GL.glLoadObj(obj, translate, scale, rotate, intensity, texture)
+
+    # fourth obj
+    obj = 'forest/rock.obj'
+    translate = (0.95, 0.2, 0.1)
+    scale = (0.1, 0.15, 0.1)
+    rotate = (0.5, 0.2, 0.3)
+    intensity = 1    
+    texture = texture_loader('forest/rock.bmp')
+    print('Rendering:   ' + obj + '\ntranslate:   ' + str(translate) + '\nscale:   ' + str(scale))
+    print('Please wait...')
+    
+    GL.glLoadObj(obj, translate, scale, rotate, intensity, texture)
+
+    GL.glFinish()
+
+    print('Output rendered to:  \'render.bmp\'')
+
 ################################################# EXAMPLES ####################################################
-high_shot()
+forest()
